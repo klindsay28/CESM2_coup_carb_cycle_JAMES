37995628f233b82c04f34c9c6e3e250d75782ff3
Keith Lindsay <klindsay@ucar.edu>
2019-11-12 06:23:08 -0700

    ensure that time is 1st dim in time:bounds when generating ann means

--- esmlab_wrap.py
+++ esmlab_wrap.py
@@ -10,6 +10,11 @@ def compute_ann_mean(ds):
     """esmlab wrapper"""
 #     return esmlab.climatology.compute_ann_mean(ds)
     ds_ann = esmlab.resample(ds, freq='ann')
+
+    tb_name = ds_ann.time.bounds
+    if ds_ann[tb_name].dims[0] != 'time':
+        ds_ann[tb_name] = ds_ann[tb_name].transpose()
+
     return ds_ann
 
 def compute_mon_anomaly(ds):
