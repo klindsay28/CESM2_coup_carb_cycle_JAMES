574e2f936f9245fc7961c16ba016e58c2e7e21b5
Keith Lindsay <klindsay@ucar.edu>
2020-01-19 07:12:43 -0700

    pytest related updates     move testing scripts to tests directory

--- run_pytest.sh
+++ run_pytest.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -i
 conda activate CESM2_coup_carb_cycle_JAMES_tst
-python -m pytest test_utils.py test_utils_units.py test_esmlab_wrap.py
-# python -m pytest test_tseries_mod.py
+python -m pytest tests/test_utils.py tests/test_utils_units.py tests/test_esmlab_wrap.py
+# python -m pytest tests/test_tseries_mod.py
