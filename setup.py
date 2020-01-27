4e35edbc05eb94d707595e8aea810fd27c618f31
Keith Lindsay <klindsay@ucar.edu>
2020-01-27 09:22:04 -0700

    add setup.py for package creation     should have occurred in 0d98a173f2cd15ac52899813ca6f3101622395fa

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    maintainer="Keith Lindsay",
+    maintainer_email="klindsay@ucar.edu",
+    description="scripts for analysis and figures for JAMES paper describing Global Carbon Cycle in CESM2",
+    name="CESM2_coup_carb_cycle_JAMES",
+    packages=["src"],
+    url="https://github.com/klindsay28/CESM2_coup_carb_cycle_JAMES",
+)
