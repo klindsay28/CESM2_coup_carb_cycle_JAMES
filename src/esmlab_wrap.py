d85b8a379a512c5b992f7f90e311d6871466c957
Keith Lindsay <klindsay@ucar.edu>
2020-02-27 15:30:32 -0700

    add kludge to esmlab_wrap:compute_mon_anomaly     workaround for problem with invalid values being passed to cftime

--- src/esmlab_wrap.py
+++ src/esmlab_wrap.py
@@ -7,6 +7,9 @@ using these wrappers eases adapting to API changes in esmlab
 from datetime import datetime, timezone
 import warnings
 
+import cftime
+import numpy as np
+
 from esmlab import resample, anomaly
 
 from src.utils import time_set_mid
@@ -58,6 +61,24 @@ def compute_ann_mean(ds):
 
 def compute_mon_anomaly(ds):
     """esmlab wrapper"""
+
+    # esmlab.anomaly generates fatal error for particular time values
+    # adjust them to avoid this
+    tb_name = ds.time.bounds
+    tb = ds[tb_name]
+    time_decoded = tb.dtype == np.dtype("O")
+    if time_decoded:
+        tb_vals_encoded = cftime.date2num(ds[tb_name].values, ds.time.encoding["units"], ds.time.encoding["calendar"])
+    else:
+        tb_vals_encoded = ds[tb_name].values
+    val0 = -0.5 / 24.0
+    tb_vals_encoded = np.where(abs(tb_vals_encoded - val0) < 1.0e-10, val0, tb_vals_encoded)
+    if time_decoded:
+        ds[tb_name].values = cftime.num2date(tb_vals_encoded, ds.time.encoding["units"], ds.time.encoding["calendar"])
+    else:
+        tb_vals_encoded = ds[tb_name].values = tb_vals_encoded
+    ds = time_set_mid(ds, "time")
+
     #     return esmlab.climatology.compute_mon_anomaly(ds)
     ds_out = anomaly(ds, clim_freq="mon")
 
